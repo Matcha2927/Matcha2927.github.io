7f1079b55a666256cf4d73433e67939952b78c94
Brandung <1539904941@qq.com>
2023-05-26 12:53:55 +0800

    Site updated: 2023-05-26 12:53:54

--- js/custom.js
+++ js/custom.js
@@ -1,7 +1,7 @@
  document.addEventListener('visibilitychange', function() {
  	var isHidden = document.hidden;
  	if (isHidden) {
- 		document.title = '你不爱我了qwq';
+ 		document.title = '你不爱我了(ó﹏ò｡)';
  	} else {
  		document.title = '抹茶蛋糕';
  	}
