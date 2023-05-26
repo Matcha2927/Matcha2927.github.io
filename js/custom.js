516b0789571570233fe05c05f124075591b9e5ff
Brandung <1539904941@qq.com>
2023-05-26 14:03:59 +0800

    Site updated: 2023-05-26 14:03:59

--- js/custom.js
+++ js/custom.js
@@ -6,3 +6,21 @@
  		document.title = '抹茶蛋糕';
  	}
  });
+
+ //躲猫猫的「猫猫」形象出自游戏「Don't catch Cats」(https://apps.apple.com/app/dont-catch-cats/id1375311035)。
+  var duoMaomao = function() {
+    var maomao = $('#maomao');
+    maomao.css('bottom', randomNum(5, 80) + 'vh');
+  }
+
+ // 创建<div>标签
+ var divElement = document.createElement("div");
+ 
+ // 设置id属性
+ divElement.setAttribute("id", "maomao");
+ 
+ // 设置onMouseOut属性
+ divElement.setAttribute("onMouseOut", "duoMaomao()");
+ 
+ // 将<div>标签添加到<body>元素中
+ document.body.appendChild(divElement);
